f6e017448e23a56d0c1d3201c4e1f73c151504a0
Soham Das <79685489+Mark-42-max@users.noreply.github.com>
2022-01-23 13:17:58 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 
-
-
 function App() {
   return (
     <div>
-
-    <Header />
-    <Features />
-    <Footer />
-
-   
-
+      <Nav />
+      <Header />
+      <Features />
+      <Footer />
     </div>
-  
-
-   
-
-
-   
-
-    );
-          
-  }
-
+  );
+}
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,63 +1,65 @@
-import React from 'react';
-import  Carousel  from 'react-bootstrap/Carousel';
-
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 const Header = () => {
   return (
-      /* Carousel Section */
-  <section>
-  <Carousel className="stylingcs">
-        <Carousel.Item >
-          
-          <div className="carouselcs">
-            <h2 >I no longer have to look around for help. I've found the best place where i could make a change.</h2>
-          </div>
-      
-          <div>
-          <img
-            className="dogcs"
-            alt="First slide"
-            src="./images/dog-img.jpg"
-          />
-          <em>Tommy, India</em>
-          </div>
-          
-        </Carousel.Item>
+    /* Carousel Section */
+    <section style={{ marginTop: "10rem" }}>
+      <Carousel className="stylingcs">
         <Carousel.Item>
-        
           <div className="carouselcs">
-            <h2 >I am so happy to be associated with the community and help animals.It is indeed a pleasure for me .</h2>
+            <h2>
+              I no longer have to look around for help. I've found the best
+              place where i could make a change.
+            </h2>
           </div>
-      
-          <div>
-          <img
-            className="dogcs"
-            alt="First slide"
-            src="./images/lady-img.jpg"
-          />
-          <em>User, India</em>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          
-          <div className="carouselcs">
-            <h2 >I am so happy to be associated with the community and help animals.It is indeed a pleasure for me .</h2>
-          </div>
-      
-          <div>
-          <img
-            className="dogcs"
-            alt="First slide"
-            src="./images/lady-img.jpg"
-          />
-          <em>User, India</em>
-          </div>
-        </Carousel.Item>
-    </Carousel>
-  </section>
 
-    
-    );
-}
+          <div>
+            <img
+              className="dogcs"
+              alt="First slide"
+              src="./images/dog-img.jpg"
+            />
+            <em>Tommy, India</em>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carouselcs">
+            <h2>
+              I am so happy to be associated with the community and help
+              animals.It is indeed a pleasure for me .
+            </h2>
+          </div>
+
+          <div>
+            <img
+              className="dogcs"
+              alt="First slide"
+              src="./images/lady-img.jpg"
+            />
+            <em>User, India</em>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carouselcs">
+            <h2>
+              I am so happy to be associated with the community and help
+              animals.It is indeed a pleasure for me .
+            </h2>
+          </div>
+
+          <div>
+            <img
+              className="dogcs"
+              alt="First slide"
+              src="./images/lady-img.jpg"
+            />
+            <em>User, India</em>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </section>
+  );
+};
 
 export default Header;
